5f11561382f878b926c5b17d389b83fbc40aa747
Marcin Felczak <feleck@gmail.com>
2017-11-13 14:39:34 +0100

    add withdraw method

--- lib/atm_machine.rb
+++ lib/atm_machine.rb
@@ -1,7 +1,17 @@
 class AtmMachine
   attr_reader :balance, :transaction_limit
+  class AccountEmptyError < StandardError
+  end
+  class NoSufficientFoundsError < StandardError
+  end
   def initialize(balance = 0)
     @balance = balance
     @transaction_limit = 500
+  end
+
+  def withdraw(amount, pin)
+    raise AccountEmptyError unless @balance > 0
+    raise NoSufficientFoundsError if amount > @balance
+    @balance -= amount
   end
 end
