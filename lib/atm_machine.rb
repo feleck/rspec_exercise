99596bab92f61dd970a67b99898a192b4468ec0a
Marcin Felczak <feleck@gmail.com>
2017-11-13 22:18:13 +0100

    add pin authorization

--- lib/atm_machine.rb
+++ lib/atm_machine.rb
@@ -4,18 +4,35 @@ class AtmMachine
   end
   class NoSufficientFoundsError < StandardError
   end
+  class InaccessiblePinError < StandardError
+  end
+  class WrongPinError < StandardError
+  end
+
   def initialize(balance = 0)
     @balance = balance
     @transaction_limit = 500
+    @pin = 1234
+    @authorized_user = false
   end
 
   def withdraw(amount, pin)
     raise AccountEmptyError unless @balance > 0
     raise NoSufficientFoundsError if amount > @balance
-    @balance -= amount
+    raise WrongPinError unless @authorized_user = @pin == pin
+    @balance -= amount if @authorized_user
   end
-  
+
   def deposit(amount, pin)
-    @balance += amount
+    raise WrongPinError unless @authorized_user = @pin == pin
+    @balance += amount if @authorized_user
+  end
+
+  def pin
+    raise InaccessiblePinError
+  end
+
+  def authorized_user
+    @authorized_user
   end
 end
